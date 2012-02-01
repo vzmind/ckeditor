dba5cc903a3dee43fee414c96f304dced98e80a7
vzmind <vzmind@gmail.com>
2012-02-01 16:59:26 +0000

    change config

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -53,13 +53,11 @@ CKEDITOR.editorConfig = function( config )
   
   config.toolbar_Easy =
     [
-        ['Source','-','Preview'],
         ['Cut','Copy','Paste','PasteText','PasteFromWord',],
         ['Undo','Redo','-','SelectAll','RemoveFormat'],
         ['Styles','Format'], ['Subscript', 'Superscript', 'TextColor'], ['Maximize','-','About'], '/',
         ['Bold','Italic','Underline','Strike'], ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
         ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-        ['Link','Unlink','Anchor'], ['Image', 'Attachment', 'Flash', 'Embed'],
-        ['Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
+        ['Link','Unlink','Anchor'], ['Image', 'Attachment'],
     ];
 };
